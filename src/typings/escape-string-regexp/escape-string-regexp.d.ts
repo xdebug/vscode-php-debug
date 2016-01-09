e75a95703e2810bf30624dc84e0c888ed558e924
Felix Becker <felix.b@outlook.com>
2016-01-09 01:25:26 +0100

    Remove escape-string-regexp typings

--- src/typings/escape-string-regexp/escape-string-regexp.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'escape-string-regexp' {
-    /** Escapes all special RegExp characters inside a string for use in the RegExp constructor */
-    function escapeStringRegexp(string: string): string;
-    export = escapeStringRegexp;
-}
