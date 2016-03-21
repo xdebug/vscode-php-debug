4ffbf5459905567aab523cddf440fd138ece5e33
Felix Becker <felix.b@outlook.com>
2016-03-21 11:35:20 +0100

    Add AppVeyor

---
+++ appveyor-install.ps1
@@ -0,0 +1,35 @@
+$ErrorActionPreference = "Stop"
+
+$client = New-Object System.Net.WebClient
+
+# Install PHP
+$phpUrl = "http://windows.php.net/downloads/releases/$env:PHP"
+Write-Output "Downloading $phpUrl"
+$client.DownloadFile($phpUrl, (Join-Path $PWD 'php.zip'))
+New-Item .\php -ItemType Directory
+Write-Output 'Extracting'
+Expand-Archive .\php.zip .\php
+Rename-Item .\php\php.ini-development php.ini
+$env:PATH += ';' + (Join-Path $PWD 'php')
+
+# Install XDebug
+$xdebugUrl = "https://xdebug.org/files/$env:XDEBUG"
+Write-Output "Downloading $xdebugUrl"
+$client.DownloadFile($xdebugUrl, (Join-Path $PWD 'xdebug.dll'))
+Move-Item .\xdebug.dll .\php\ext
+Add-Content .\php\php.ini @"
+extension_dir=ext
+zend_extension=xdebug.dll
+xdebug.remote_enable=1
+xdebug.remote_autostart=1
+"@
+
+# Install Node
+Write-Output 'Installing Node'
+Install-Product node 4.1.1 x64
+npm install -g typings
+
+# Install dependencies
+Write-Output 'Installing dependencies'
+npm install
+typings install
