6b6400787c1191590bb9b80687393c78e15bab65
Felix Becker <felix.b@outlook.com>
2016-03-28 00:34:23 +0200

    Use debugadapter-testsupport's new defaultTimeout

--- src/test/adapter.ts
+++ src/test/adapter.ts
@@ -14,6 +14,7 @@ describe('PHP Debug Adapter', () => {
 
     beforeEach('start debug adapter', async () => {
         client = new DebugClient('node', path.normalize(__dirname + '/../phpDebug'), 'php');
+        client.defaultTimeout = 10000;
         await client.start(process.env.VSCODE_DEBUG_PORT && parseInt(process.env.VSCODE_DEBUG_PORT));
     });
 
