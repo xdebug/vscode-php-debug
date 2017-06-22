c221ca9a41feb30f42bf941a53e4f0d7558bcc39
Felix Becker <felix.b@outlook.com>
2017-06-22 16:10:09 +0200

    Fallback to archives if AppVeyor PHP download fails with 404 (#170)

--- appveyor-install.ps1
+++ appveyor-install.ps1
@@ -2,8 +2,21 @@ $client = New-Object System.Net.WebClient
 
 # Install PHP
 $phpUrl = "http://windows.php.net/downloads/releases/php-$env:PHP_VERSION-nts-Win32-VC$env:VC_VERSION-x86.zip"
+$target = Join-Path $PWD 'php.zip'
 Write-Output "Downloading $phpUrl"
-$client.DownloadFile($phpUrl, (Join-Path $PWD 'php.zip'))
+try {
+    $client.DownloadFile($phpUrl, $target)
+} catch [System.Net.WebException] {
+    if ($_.Exception.Response.StatusCode.Value__ -eq 404) {
+        # Older releases get moved to archives/
+        $phpUrl = "http://windows.php.net/downloads/releases/archives/php-$env:PHP_VERSION-nts-Win32-VC$env:VC_VERSION-x86.zip"
+        Write-Output "Downloading $phpUrl"
+        $client.DownloadFile($phpUrl, $target)
+        Write-Warning "PHP $env:PHP_VERSION is outdated and was moved to archives"
+    } else {
+        throw $_
+    }
+}
 7z e php.zip -ophp
 Rename-Item .\php\php.ini-development php.ini
 $env:PATH += ';' + (Join-Path $PWD 'php')
