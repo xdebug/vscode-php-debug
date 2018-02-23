ecb4199a54133abd151a47d17f9422e83e4ecf54
Felix Becker <felix.b@outlook.com>
2018-02-23 11:50:02 -0800

    ci(appveyor): improve install script error handling

--- appveyor-install.ps1
+++ appveyor-install.ps1
@@ -1,23 +1,29 @@
-$client = New-Object System.Net.WebClient
+$ErrorActionPreference = 'Stop'
 
 # Install PHP
+$client = New-Object System.Net.WebClient
 $phpUrl = "http://windows.php.net/downloads/releases/php-$env:PHP_VERSION-nts-Win32-VC$env:VC_VERSION-x86.zip"
 $target = Join-Path $PWD 'php.zip'
 Write-Output "Downloading $phpUrl"
 try {
     $client.DownloadFile($phpUrl, $target)
-} catch [System.Net.WebException] {
+}
+catch [System.Net.WebException] {
     if ($_.Exception.Response.StatusCode.Value__ -eq 404) {
         # Older releases get moved to archives/
         $phpUrl = "http://windows.php.net/downloads/releases/archives/php-$env:PHP_VERSION-nts-Win32-VC$env:VC_VERSION-x86.zip"
         Write-Output "Downloading $phpUrl"
         $client.DownloadFile($phpUrl, $target)
         Write-Warning "PHP $env:PHP_VERSION is outdated and was moved to archives"
-    } else {
+    }
+    else {
         throw $_
     }
 }
 7z e php.zip -ophp
+if ($LASTEXITCODE -ne 0) {
+    throw "7zip exited with $LASTEXITCODE"
+}
 Rename-Item .\php\php.ini-development php.ini
 $env:PATH += ';' + (Join-Path $PWD 'php')
 
