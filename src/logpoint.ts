138413875fd4375aba99e5a943cc34f06f436755
Damjan Cvetko <damjan.cvetko@monotek.net>
2021-07-29 15:35:14 +0200

    Implemented logpoint support.

--- src/logpoint.ts
+++ src/logpoint.ts
@@ -1,9 +1,13 @@
 import stringReplaceAsync = require('string-replace-async')
+import { isWindowsUri } from './paths'
 
 export class LogPointManager {
     private _logpoints = new Map<string, Map<number, string>>()
 
     public addLogPoint(fileUri: string, lineNumber: number, logMessage: string) {
+        if (isWindowsUri(fileUri)) {
+            fileUri = fileUri.toLowerCase()
+        }
         if (!this._logpoints.has(fileUri)) {
             this._logpoints.set(fileUri, new Map<number, string>())
         }
@@ -11,22 +15,39 @@ export class LogPointManager {
     }
 
     public clearFromFile(fileUri: string) {
+        if (isWindowsUri(fileUri)) {
+            fileUri = fileUri.toLowerCase()
+        }
         if (this._logpoints.has(fileUri)) {
             this._logpoints.get(fileUri)!.clear()
         }
     }
 
     public hasLogPoint(fileUri: string, lineNumber: number): boolean {
-        return this._logpoints.has(fileUri) && this._logpoints.get(fileUri)!.has(lineNumber);
+        if (isWindowsUri(fileUri)) {
+            fileUri = fileUri.toLowerCase()
+        }
+        return this._logpoints.has(fileUri) && this._logpoints.get(fileUri)!.has(lineNumber)
     }
 
-    public async resolveExpressions(fileUri: string, lineNumber: number, callback: (expr: string) => Promise<string>): Promise<string> {
-        if(!this.hasLogPoint(fileUri, lineNumber)) {
+    public async resolveExpressions(
+        fileUri: string,
+        lineNumber: number,
+        callback: (expr: string) => Promise<string>
+    ): Promise<string> {
+        if (isWindowsUri(fileUri)) {
+            fileUri = fileUri.toLowerCase()
+        }
+        if (!this.hasLogPoint(fileUri, lineNumber)) {
             return Promise.reject('Logpoint not found')
         }
         const expressionRegex = /\{(.*?)\}/gm
-        return await stringReplaceAsync(this._logpoints.get(fileUri)!.get(lineNumber), expressionRegex, function(_: string, group: string) {
-            return callback(group);
-        });
+        return await stringReplaceAsync(
+            this._logpoints.get(fileUri)!.get(lineNumber)!,
+            expressionRegex,
+            function (_: string, group: string) {
+                return group.length === 0 ? Promise.resolve('') : callback(group)
+            }
+        )
     }
 }
