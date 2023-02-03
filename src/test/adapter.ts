93c84ba47789d2fc86aba21f8bf6b2ca0a74f895
8ctopus <hello@octopuslabs.io>
2023-02-03 12:15:40 +0400

    Add ignore exception test

--- src/test/adapter.ts
+++ src/test/adapter.ts
@@ -212,6 +212,12 @@ describe('PHP Debug Adapter', () => {
                 await Promise.all([client.configurationDoneRequest(), client.waitForEvent('terminated')])
             })
 
+            it('should not break on exception that matches the ignore pattern', async () => {
+                await client.launch({ path.join(TEST_PROJECT, 'ignore_exception.php'), ignoredException: ['IgnoredException'] })
+                await client.setExceptionBreakpointsRequest({ filters: ['*'] })
+                await Promise.all([client.configurationDoneRequest(), client.waitForEvent('terminated')])
+            })
+
             it('should support stopping only on a notice', async () => {
                 await client.launch({ program })
                 await client.setExceptionBreakpointsRequest({ filters: ['Notice'] })
