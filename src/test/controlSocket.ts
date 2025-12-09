5a37ab71de790df63fb5b0d91c5d73fd00fe62be
Damjan Cvetko <damjan.cvetko@gmail.com>
2025-12-09 23:23:45 +0100

    Prettier

--- src/test/controlSocket.ts
+++ src/test/controlSocket.ts
@@ -3,11 +3,9 @@ import { ControlSocket } from '../controlSocket'
 import { assert } from 'chai'
 
 describe('ControlSocket', () => {
-
     it('should try to get list of sockets', async () => {
         const cs = new ControlSocket()
         const r = await cs.listControlSockets()
         assert.isArray(r)
     })
-
 })
